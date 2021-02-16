3c149be265c0fb9dec23a29a823bcb3f559168dc
jh27kim <jh27kim@gmail.com>
2021-02-16 23:11:00 +0900

    Version 0.0.3

--- hyuweb_v1/common/views.py
+++ hyuweb_v1/common/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from common.forms import UserForm
 
-from django.urls import path
-from . import views
 
-# Create your views here.
-
+def signup(request):
+    if request.method == "POST":
+        forms = UserForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            username = forms.cleaned_data.get('username')
+            raw_password = forms.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        forms = UserForm()
+    return render(request, 'common/signup.html', {'form': forms})
--- hyuweb_v1/config/urls.py
+++ hyuweb_v1/config/urls.py
@@ -1,9 +1,10 @@
-
+from pride import views
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('pride/', include('pride.urls')),
     path('common/', include('common.urls')),
 ]
